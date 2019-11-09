d3491e57ccc1cfd41e93d834d0614dc34c3d6ad3
Uchey <ucheuzochukwumark@gmail.com>
2019-11-09 14:00:18 +0100

    Updates

---
+++ precache-manifest.5039a63a148c553f088dd5e09a8dbe28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d239c443defa563e8b6847aa275eee7",
+    "url": "/Burger-App-React-/index.html"
+  },
+  {
+    "revision": "541c15c4ef56823b5b5c",
+    "url": "/Burger-App-React-/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a6ad365ee6522a153d44",
+    "url": "/Burger-App-React-/static/js/2.69f0873e.chunk.js"
+  },
+  {
+    "revision": "541c15c4ef56823b5b5c",
+    "url": "/Burger-App-React-/static/js/main.4a54eaac.chunk.js"
+  },
+  {
+    "revision": "185b3d8e09c586b23c19",
+    "url": "/Burger-App-React-/static/js/runtime-main.e1d337ce.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Burger-App-React-/static/media/logo.25bf045c.svg"
+  }
+]);
