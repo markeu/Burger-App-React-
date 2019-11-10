042f350b6f902f1cc69057ff7a040f4c57ee9fcb
Uchey <ucheuzochukwumark@gmail.com>
2019-11-10 15:56:21 +0100

    finalize component layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Aux from '../../hoc/auxillary'
+import Aux from '../../hoc/auxillary';
+import classes from './Layout.module.css'
 
 const Layout = ( props ) => (
     <Aux>
         <div>Toolbar, Backdrop,  sidedrawer </div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
