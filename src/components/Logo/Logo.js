c0f75fd72a380949c66be93615d5b7934f0bc8c0
Uchey <ucheuzochukwumark@gmail.com>
2019-11-12 21:46:10 +0100

    responsive adjutments for the sidedrawer

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import BurgerLogo from '../../assets/Images/27.1 burger-logo.png.png';
 import classes from './Logo.module.css';
 
 const logo = (props) => (
-    <div className={classes.Logo}>
+    <div className={classes.Logo}  style={{height: props.height}}>
         <img src={BurgerLogo} alt={'MyBurger'}/>
     </div>
 );
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/auxillary'
+
+const SideDrawer = () => {
+  return (
+      <Aux>
+          <Backdrop show />
+        <div className={classes.SideDrawer}>
+            <div className={classes.Logo}>
+                < Logo />
+            </div>
+            <nav >
+                <NavigationItems />
+            </nav>
+        </div>
+      </Aux>
+  )
+}
+
+export default SideDrawer
