0fca34063061c8b4b2f9a8a3ae0f9432e13774bf
Uchey <ucheuzochukwumark@gmail.com>
2019-11-18 11:13:15 +0100

    unmouting of old interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,15 +8,19 @@ const withErrorHandler = ( WrapperComponent, axios ) => {
             error: null
         }
             componentWillMount(){
-                axios.interceptors.request.use(req => {
+                this.reqInterceptors = axios.interceptors.request.use(req => {
                     this.setState({error: null});
                     return req;
                 })
-                axios.interceptors.response.use(res => res, error => {
+                this.resInterceptors = axios.interceptors.response.use(res => res, error => {
                     this.setState({error: error})
                 })
             }
 
+            componentWillUnmount(){
+                axios.interceptors.request.eject(this.reqInterceptors);
+                axios.interceptors.response.eject(this.resInterceptors);
+            }
             errorConfirmHandler = () => {
                 this.setState({error: null})
             }
