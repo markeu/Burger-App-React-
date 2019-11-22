8fc20b288d46408893de1c2319f95097e307d4f3
Uchey <ucheuzochukwumark@gmail.com>
2019-11-22 15:25:22 +0100

    finalise order page and database intergration

---
+++ src/components/Order/Order.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from './Order.module.css';
+
+const Order = (props) => {
+    const ingredients = [];
+
+    for (let IngredientName in props.ingredients) {
+        ingredients.push(
+            {
+                name: IngredientName,
+                amount:props.ingredients[IngredientName]
+            })
+    }
+
+    const ingredientsOutput = ingredients.map( ig => {
+        return <span
+         style={{textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          padding: '5px',
+          border: '1px solid #ccc' }}
+         key={ig.name}>{ig.name} ({ig.amount})</span>
+    })
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients: {ingredientsOutput}</p>
+      <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+    </div>
+  )
+}
+
+export default Order
