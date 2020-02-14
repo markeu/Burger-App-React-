ed408282e8e5bec8a00a73baeb4d935123a3928f
Uchey <ucheuzochukwumark@gmail.com>
2020-02-14 09:32:19 +0100

    heroku deploy

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -64,7 +64,7 @@ class BurgerBuilder extends Component{
     //     const newPrice = oldPrice - priceDeduction;
     //     this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
     //     this.updatePurchaseState(updatedIngredients)
-    // };
+    
 
     purchaseHandler = () => {
         this.setState({purchasing: true})
